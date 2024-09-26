a7e1e9e66da2ddb0cfa8707602c354828b262ed6
Pibis <87157662+PibisFunny@users.noreply.github.com>
2024-09-25 22:39:40 -0700

    jamie interaction

---
+++ scripts/imageChange.js
@@ -0,0 +1,17 @@
+function changeImg(objImg, newSrc) {
+  const originalSrc = objImg.getAttribute('src');
+  let originalSrcBase = objImg.getAttribute('data-original-src');
+  
+  if (!originalSrcBase) {
+    objImg.setAttribute('data-original-src', originalSrc);
+    originalSrcBase = originalSrc; // Update the originalSrcBase variable
+  }
+  
+  if (originalSrc === originalSrcBase) {
+    objImg.setAttribute('src', newSrc);
+    
+    setTimeout(() => {
+      objImg.setAttribute('src', originalSrcBase);
+    }, 500);
+  }
+}
